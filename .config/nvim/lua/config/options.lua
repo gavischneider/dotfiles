481b0575e94f32f4c0395cb125bb3ad578a9ebb3
Gavi Schneider <gavischneider@gmail.com>
2025-09-04 19:02:01 +0300

    Add initial neovim boilerplate

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Set leader key
+vim.g.mapleader = ' '
+
+-- Enable line numbers
+vim.opt.number = true
+
+-- Set tab and indent settings
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
