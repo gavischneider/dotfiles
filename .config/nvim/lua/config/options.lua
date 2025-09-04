488dfa20ad160e0e9ac88b8642b777645b37d0e1
Gavi Schneider <gavischneider@gmail.com>
2025-09-04 19:49:32 +0300

    add more options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 -- Set leader key
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- Enable line numbers
 vim.opt.number = true
@@ -10,3 +10,15 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+
+-- Enable mouse support
+vim.opt.mouse = "a"
+
+-- Clipboard integration
+vim.opt.clipboard:append({"unnamed", "unnamedplus"})
+
+-- Display settings
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
