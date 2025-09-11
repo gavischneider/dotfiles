6aaec2ef7b6d847dcc77caa4fa5d44a635da925d
Gavi Schneider <gavischneider@gmail.com>
2025-09-11 12:21:28 +0300

    Add nvim-tree

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,46 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+        local nvimtree = require("nvim-tree")
+
+        -- Disable netrw
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        nvimtree.setup({
+            view = {
+                width = 30,
+                cursorline = true,
+                side = "left",
+            },
+            renderer = {
+                icons = {
+                    web_devicons = {
+                        file = {
+                            enable = true,
+                            color = true,
+                        },
+                        folder = {
+                            enable = false,
+                            color = true,
+                        },
+                    },
+                },
+            },
+            filters = {
+                dotfiles = false,
+            },
+            git = {
+                ignore = false,
+            },
+        })
+
+        vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+        vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>",
+            { desc = "Toggle file explorer on current file" })
+        vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+        vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+        vim.keymap.set("n", "<C-d>", "<C-w>w", { desc = "Switch window" })
+    end
+}
