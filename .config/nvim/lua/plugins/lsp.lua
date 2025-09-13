006d8fb51e2b4bc9a96aebf1b79bcfb9e8acf274
Gavi Schneider <gavischneider@gmail.com>
2025-09-13 23:12:40 +0300

    Add formatting keymap in lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,19 +1,22 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        {
-            "folke/lazydev.nvim",
-            ft = "lua", -- only load on lua files
-            opts = {
-                library = {
-                    -- See the configuration section for more details
-                    -- Load luvit types when the `vim.uv` word is found
-                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                },
-            },
-        },
-    },
-    config = function()
-        require("lspconfig").lua_ls.setup{}
-    end
+  "neovim/nvim-lspconfig",
+  dependencies = { {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = { -- See the configuration section for more details
+      -- Load luvit types when the `vim.uv` word is found
+      {
+        path = "${3rd}/luv/library",
+        words = { "vim%.uv" }
+      } }
+    }
+  } },
+  config = function()
+    require("lspconfig").lua_ls.setup{}
+    -- Format file
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format()
+    end)
+  end
 }
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,10 +1,10 @@
 return {
-    "nvim-mini/mini.nvim",
-    version = "*",
-    config = function()
-        -- Configuration for individual mini.nvim modules goes here
-        -- statusline
-        local statusline = require"mini.statusline"
-        statusline.setup{ use_icons = true }
-    end
+  "nvim-mini/mini.nvim",
+  version = "*",
+  config = function()
+    -- Configuration for individual mini.nvim modules goes here
+    -- statusline
+    local statusline = require"mini.statusline"
+    statusline.setup{ use_icons = true }
+  end
 }
